80134d06be39444a14bbdb5a82da705d476405e3
Bukitei <55440174+Bukitei@users.noreply.github.com>
2019-11-07 12:33:28 +0000

    first commit

---
+++ src/main/java/dad/javafx/geolocalization/client/GeoServicexception.java
@@ -0,0 +1,30 @@
+package dad.javafx.geolocalization.client;
+
+@SuppressWarnings("serial")
+public class GeoServicexception extends Exception {
+
+	public GeoServicexception() {
+		
+	}
+
+	public GeoServicexception(String message) {
+		super(message);
+		
+	}
+
+	public GeoServicexception(Throwable cause) {
+		super(cause);
+		
+	}
+
+	public GeoServicexception(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	public GeoServicexception(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		
+	}
+
+}
