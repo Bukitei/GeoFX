59810fb5ac1e6d58351adefff2d9eaad6b98624f
Bukitei <borjagomezb@gmail.com>
2019-12-09 14:42:18 +0100

    terminado

--- src/main/java/dad/javafx/geolocalization/controller/LocationController.java
+++ src/main/java/dad/javafx/geolocalization/controller/LocationController.java
@@ -2,7 +2,6 @@ package dad.javafx.geolocalization.controller;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import dad.javafx.geolocalization.models.LocationModel;
